516408650979c881d746c950fa7a374737d9ff61
kaushaldeokar <kaushal.deokar@gmail.com>
2023-05-12 13:14:07 +0530

    Box Moving React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React,{useState} from "react";
+
 
 function App() {
+  let [l,setL]=useState('0');
+  let [t,setT]=useState('0');
+
+   const lF=(a)=>{
+    let k=Number(l);
+    if(k===0 && a===-50)return;
+    if(k===450 && a===50)return;
+    k+=a;
+    setL(String(k));
+    console.log("l is ", l);
+  }
+  const Tf=(a)=>{
+    let k=Number(t);
+    if(k===0 && a===-50)return;
+    if(k===450 && a===50)return;
+    k+=a;
+    setT(String(k));
+    console.log("t is ", t);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="main_container">
+        <button className="horizontal" onClick={()=>Tf(-50)}>
+          Up
+        </button>
+        <div className="container">
+          <button className="vertical"  onClick={()=>lF(-50)}>
+            Left
+          </button>
+          <div className="main">
+
+            <div className="box" style={{left:`${l}px`,top:`${t}px`}}></div>
+            
+          </div>
+          <button className="vertical" onClick={()=>lF(50)} >Right</button>
+        </div>
+        <button className="horizontal" onClick={()=>Tf(50)}>Bottom</button>
+        {/* {l},{t} */}
+      </div>
+    
   );
 }
 
---
+++ src/Box.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Box = (props) => {
+  return (
+    
+      <div className="box" style={{left:props.l,top:props.t}}></div>
+    
+  )
+}
+
+export default Box
